2a3f228463b4236765a22c494da6d562bbc32a4d
FelipeMaciasAgogo <pipemaciasmoncada5@gmail.com>
2024-11-12 15:54:34 -0500

    Se añaden nuevas particularidades
    Se añade imagen de felipe macias,tambien se modifica la pagina visual para que tenga otro aspecto mas bonito

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const faders = document.querySelectorAll('.fade-in');
-    
-    const appearOptions = {
-        threshold: 0.5, 
-        rootMargin: "0px 0px -50px 0px"
-    };
-    
-    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
-        entries.forEach(entry => {
-            if (!entry.isIntersecting) {
-                return;
-            } else {
-                entry.target.classList.add('show'); 
-                appearOnScroll.unobserve(entry.target); 
-            }
-        });
-    }, appearOptions);
-    
-    faders.forEach(fader => {
-        appearOnScroll.observe(fader);
+    document.querySelectorAll('.fade-in').forEach(element => {
+        element.classList.add('show');
     });
 });
